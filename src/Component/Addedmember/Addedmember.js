f25ebfeafa30f19721f6344ef1461384ab858745
itzrizvi <shahriar.me.work@gmail.com>
2021-09-26 21:11:14 +0600

    List Changes

--- src/Component/Addedmember/Addedmember.js
+++ src/Component/Addedmember/Addedmember.js
@@ -13,13 +13,15 @@ const Addedmember = (props) => {
     return (
         <div className="member-list">
             <h2>Total Summery</h2>
-            <h3>Total Added: <span>{props.addmember.length} Members</span></h3>
+            <h3>Total Added: <span>{props.addmember.length} Member</span></h3>
             <h3>Total Cost: <span>${totalSalary}</span></h3>
             <h3>Member List:</h3>
             <ul id="list">
                 {
                     // Using Map for list of names added
-                    addmember.map(singlemember => <li key={singlemember.id}>{singlemember.name}</li>)
+                    addmember.map(singlemember => <li key={singlemember.id}>
+                        <img src={singlemember.img} alt="" />
+                        {singlemember.name}</li>)
                 }
             </ul>
         </div>
